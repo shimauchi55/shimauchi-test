cb7e705ac55710ad389e454d2aeea85482431308
島内 鉄矢 <shimauchi@tagbangers.co.jp>
2020-03-04 17:59:09 +0900

    hoge

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hogehoge");
+		System.out.println("hogehogehoge");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
